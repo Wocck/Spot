cc7cac8a25b65709bc9bd0cae26f7b56b289c13a
wsek <wsek@atinea.pl>
2024-05-27 09:06:33 +0100

    added simple demo app

---
+++ Spot/src/main/java/com/wocck/spot/HelloController.java
@@ -0,0 +1,18 @@
+package com.wocck.spot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hi() {
+        return "Hello World!";
+    }
+}
