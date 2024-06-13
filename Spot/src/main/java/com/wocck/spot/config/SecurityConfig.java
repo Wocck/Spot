abaffa159998f01dafaaa1878e338d0aea8c487c
wsek <wsek@atinea.pl>
2024-06-13 15:17:13 +0100

    working redirections

--- Spot/src/main/java/com/wocck/spot/config/SecurityConfig.java
+++ Spot/src/main/java/com/wocck/spot/config/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/", "/index", "/home", "/error", "/login**", "/callback**", "/webjars/**", "/error**").permitAll()
+                        .requestMatchers("/", "/index", "/home", "/error", "/login**", "/callback**", "/webjars/**", "/error**", "/WEB-INF/views/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2Login -> oauth2Login
@@ -26,5 +29,10 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/")
                 );
         return http.build();
+    }
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
     }
 }
